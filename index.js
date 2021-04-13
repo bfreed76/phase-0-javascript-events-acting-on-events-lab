aa5aba1be2401acaa84122babad5f2833ce1cd83
Brinton <brintonfoyreed@gmail.com>
2021-04-13 11:15:21 -0700

    commit tree a642678d3a54962401d78f887c6df81845c80fce

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+const game = document.getElementById("game");
+
+dodger.style.backgroundColor = "pink";
+game.style.backgroundColor = 'blue';
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }};
+
+   
+
+function moveDodgerRight(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+
+    if (left < 255) {
+        dodger.style.left = `${left +1}px`
+    }
+};
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } if (e.key === "ArrowRight") {
+        moveDodger
+    }
+  }); 
+          
